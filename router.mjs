a0e65acc94a0c73b624343c44e86bb78843d7216
iamphelipe <phelipeguimaraes2023@gmail.com>
2026-01-02 01:10:08 -0300

    feat: add features routes

--- router.mjs
+++ router.mjs
@@ -1,15 +1,20 @@
-export const routes = {
-    GET: {
-        '/': (req, res) => {
-            res.end('Home')
-        },
-        '/produto/notebook': (req, res) => {
-            res.end('Produtos - Notebook')
-        }
-    },
-    POST: {
-        '/produto': (req, res) => {
-            res.end('Notebook Post')
-        }
-    } 
+export class Router {
+
+    routes = {
+        GET: {},
+        POST: {}
+    }
+
+    get(route, handler) {
+        this.routes["GET"][route] = handler
+    }
+
+    post(route, handler) {
+        this.routes["POST"][route] = handler
+    }
+
+    find(method, route){
+        return this.routes[method]?.[route] || null;
+    }
+
 }
--- server.mjs
+++ server.mjs
@@ -1,8 +1,27 @@
 import { createServer } from "node:http";
-import { routes } from "./router.mjs";
+import { Router } from "./router.mjs";
+
+const router = new Router();
+
+router.get('/', (req, res) => {
+    res.end('Home')
+})
+
+router.get('/produto/notebook', (req, res) => {
+    res.end('Produtos - Notebook')
+})
+
+router.get('/contato', (req, res) => {
+    res.end('Contato')
+})
+
+router.post('/produto', (req, res) => {
+    res.end('Notebook Post')
+})
+
+console.log(router.routes);
 
 const server = createServer( async (req, res) => {
-    res.statusCode = 200;
 
     const url = new URL(req.url, 'http://localhost');
 
@@ -12,8 +31,9 @@ const server = createServer( async (req, res) => {
     }
 
     const body = Buffer.concat(chunks).toString("utf-8");
-    const handler = routes[req.method][url.pathname];
+    const handler = router.find(req.method, url.pathname)
     if(handler) {
+        res.statusCode = 200;
         handler(req, res)
     } else {
         res.statusCode = 404;
