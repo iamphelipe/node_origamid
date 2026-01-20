341f7ab171cf536f06fd9929e0ca4bf54c288856
iamphelipe <phelipeguimaraes2023@gmail.com>
2026-01-19 22:37:23 -0300

    feat: route-error

--- core/core.ts
+++ core/core.ts
@@ -3,46 +3,63 @@ import { Router } from "./router.ts";
 import { customRequest } from "./htpp/custom-request.ts";
 import { customResponse } from "./htpp/custom-response.ts";
 import { bodyJson } from "./middleware/body-json.ts";
+import { RouteError } from "./utils/route-error.ts";
 
 export class Core {
+  router: Router;
+  server: Server;
+  constructor() {
+    this.router = new Router();
+    this.router.use([bodyJson]);
+    this.server = createServer(this.handler);
+  }
 
-    router: Router
-    server: Server
-    constructor() {
-        this.router = new Router();
-        this.router.use([bodyJson]);
-        this.server = createServer(this.handler);
-    }
+  handler = async (request: IncomingMessage, response: ServerResponse) => {
+    try {
+      const req = await customRequest(request);
+      const res = customResponse(response);
 
-    handler = async (request: IncomingMessage, response: ServerResponse) => {
-        const req = await customRequest(request);
-        const res = customResponse(response);
+      for (const middleware of this.router.middlewares) {
+        await middleware(req, res);
+      }
 
-        for(const middleware of this.router.middlewares) {
-            await middleware(req, res)
+      const matched = this.router.find(
+        (req.method as "GET" | "POST") || "",
+        req.pathname,
+      );
+      if (!matched) {
+        throw new RouteError(404, 'nao encontrada');
+      }
+
+      const { route, params } = matched;
+      req.params = params;
+
+      for (const middleware of route.middlewares) {
+        await middleware(req, res);
+      }
+
+      route.middlewares;
+
+      await route.handler(req, res);
+    } catch (error) {
+        if(error instanceof RouteError) {
+            console.error(`${error.status} ${error.message} | ${request.method} ${request.url}`);
+            response.statusCode = error.status;
+            response.setHeader('content-type', 'application/problem+json');
+            response.end(JSON.stringify({status: response.statusCode, title: error.message}));
+        } else {
+            console.error(error);
+            response.statusCode = 500;
+            response.setHeader('content-type', 'application/problem+json');
+            response.end(JSON.stringify({status: response.statusCode, title: "error"}));
+
         }
-
-        const matched = this.router.find(req.method as "GET" | "POST" || "", req.pathname);
-        if(!matched) {
-            return res.status(404).end('nao encontrada');
-        }
-
-        const { route, params } = matched;
-        req.params = params;
-
-        for(const middleware of route.middlewares) {
-            await middleware(req, res)
-        }
-
-        route.middlewares
-
-        await route.handler(req, res)
-
     }
+  };
 
-    init() {
-        this.server.listen(3000, () => {
-            console.log("Server: http://localhost:3000");
-        })
-    }
+  init() {
+    this.server.listen(3000, () => {
+      console.log("Server: http://localhost:3000");
+    });
+  }
 }
--- index.ts
+++ index.ts
@@ -1,24 +1,22 @@
 
 import { Core } from "./core/core.ts";
 import { getCourseSlug } from "./core/database.ts";
-import { bodyJson } from "./core/middleware/body-json.ts";
 import { logger } from "./core/middleware/logger.ts";
+import { RouteError } from "./core/utils/route-error.ts";
 
 const core = new Core;
 
 core.router.use([logger])
 
 core.router.get("/curso/:slug", (req, res) => {
+  const { slug } = req.params;
+  const curso = getCourseSlug(slug);
 
-    const { slug } = req.params;
-    const curso = getCourseSlug(slug);
-    
-    if(curso) {
-        res.status(200).json(curso);
-    } else {
-        res.status(404).json("curso não encontrado!")
-    }
+  if (!curso) {
+    throw new RouteError(404, "curso nao encontrado");
+  }
 
+  res.status(200).json(curso);
 });
 
 core.router.get("/", (req, res) => {
